cbc99685d6d83141fa88978873e7b7c4ada64252
lepoanda <leopranandodcj1998@gmail.com>
2023-05-31 07:28:52 +0700

    final project

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//Toggle class Active
+
+const navbarNav = document.querySelector(".navbar-nav");
+
+//ketika menu diklik
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//klik diluar sidebar menghilangkan menu
+const menu = document.querySelector("#menu");
+
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
